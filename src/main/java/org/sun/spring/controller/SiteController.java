c06a575cebac65a5b93269b70d3ccf18b31dc671
tianfei <tianfei@wusongtech.com>
2016-10-17 09:58:39 +0800

    push all

--- src/main/java/org/sun/spring/controller/SiteController.java
+++ src/main/java/org/sun/spring/controller/SiteController.java
@@ -3,6 +3,7 @@ package org.sun.spring.controller;
 import java.util.HashMap;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,11 @@ public class SiteController {
         HashMap<String, Object> result = new HashMap<String, Object>();
         result.put("code", 1);
         return result;
+    }
+
+    @RequestMapping("/hello")
+    public String testString(@RequestParam(name = "userName",required = false) String userName){
+        return userName==null? "Hello man" :"Hello "+userName;
     }
 
 }
