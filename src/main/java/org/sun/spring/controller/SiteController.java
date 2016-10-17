188ec65b7c0a730239cd3f04328c8d3cec5d135a
tianfei <tianfei@wusongtech.com>
2016-10-17 14:11:50 +0800

    remove eclipse file

--- src/main/java/org/sun/spring/controller/SiteController.java
+++ src/main/java/org/sun/spring/controller/SiteController.java
@@ -1,24 +1,43 @@
 package org.sun.spring.controller;
 
 import java.util.HashMap;
+import java.util.Map;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SiteController {
 
-    @RequestMapping("/code")
-    public HashMap<String, Object> actionIndex() {
+    @RequestMapping(value = "/code/{codeNo}",
+            method = RequestMethod.DELETE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, //要求请求头必须有MIME类型
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE //返回的MIME数据类型
+    )
+    public HashMap<String, Object> actionIndex(@PathVariable("codeNo") Integer codeNo) {
         HashMap<String, Object> result = new HashMap<String, Object>();
-        result.put("code", 1);
+        result.put("code", codeNo);
         return result;
     }
 
-    @RequestMapping("/hello")
-    public String testString(@RequestParam(name = "userName",required = false) String userName){
-        return userName==null? "Hello man" :"Hello "+userName;
+
+
+
+    //region Description
+    /*@RequestMapping(value = "/code/{codeNo}")
+    public Map<String, Object> code(@PathVariable("codeNo") Integer codeNo) {
+        HashMap<String, Object> result = new HashMap<String, Object>();
+        result.put("code", codeNo);
+        return result;
+    }*/
+    //endregion
+
+    //<editor-fold desc="Description">
+    @RequestMapping(value = "/hello",
+            method = RequestMethod.GET)
+    public String testString(@RequestParam(name = "userName", required = false) String userName) {
+        return userName == null ? "Hello man" : "Hello " + userName;
     }
+    //</editor-fold>
 
 }
